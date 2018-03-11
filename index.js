037e0e002bf2e9ea5a79ab3715165596686c8fa8
Jairo Espinosa <jairoespinosa95@gmail.com>
2018-03-11 22:04:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,3 +10,18 @@ function distanceFromHqInFeet(num){
 function distanceTravelledInFeet(b1, b2){
   return Math.abs((b1-b2)*264);
 }
+
+function calculatesFarePrice(start, destination) {
+  let distance = distanceTravelledInFeet(start, destination);
+
+  if (distance < 400) {
+    return 0;
+  } else if (distance < 2000) {
+    let distance = distance - 400;
+    return distance * 0.02;
+  } else if (distance> 2000 && distance < 2500){
+    return 25;
+  }  else {
+    return "cannot travel that far."
+  }
+}
